9d574a331bc5ecb6afde8263815bb7db4f8713d6
fimaAidarous <fma0899@gmail.com>
2023-12-25 02:11:19 -0800

    fit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express, { request, response } from "express"
-import {PORT, mongoDBURL} from "./config.js";
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from './models/bookmodels.js';
+import { Book } from "./models/bookmodels.js";
 
 const app = express();
 
@@ -9,111 +9,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request,response) =>{
-   console.log(request)
-   return request.status(234).send('Welcome to MERN Stack Tutorial') 
+app.get("/", (request, response) => {
+  console.log(request);
+  return request.status(234).send("Welcome to MERN Stack Tutorial");
 });
 
-//Route for save
-app.post('/books', async (request,response) => {
- try{
-    if(
-        !request.body.title ||
-        !request.body.author ||
-        !request.body.publisherYear
-    )
-    {
-        return response.status(400).send({
-            message:'Send all required fileds:title,author,publisher',
-        });
-    }
-    const newBook = {
-        title:request.body.title,
-        author:request.body.author,
-        publisherYear:request.body.publisherYear,
-    };
-    const book = await Book.create(newBook);
-    return response.status(201).send(book);
- }catch (error) {
-    console.log(error.message);
-    response.status(500).send({message:error.message});
- }
-});
-
-//Route for Get All Books from database
-app.get('/books', async(request,response) => {
-    try{
-        const books = await Book.find({});
-
-    return response.status(200).json({
-        count: books.length,
-        data:books
-    });
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message:error.message});
-    }
-});
-
-//Route for Get All Books from database by Id
-app.get('/books/:id', async(request,response) => {
-    try{
-        const { id } = request.params;
-        const book = await Book.findById(id);
-
-    return response.status(200).json(book)
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message:error.message});
-    }
-});
-//Route for Update a Book
-app.put('/books/:id', async (request,response) =>{
-    try{
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publisherYear
-        ){
-            return response.status(400).send({
-                message:'Send all required fileds: title,author,publisherYear',
-            });
-        }
-        const { id } = request.params;
-        const result = await Book.findByIdAndUpdate(id,request.body);
-        if(!result){
-            return response.status(404).json({message:"Book not found"});
-        }
-        return response.status(200).send({message:'Book updated successfully'});
-
-    }catch (error){
-        console.log(error.message);
-        response.status(500).send({message:error.message})
-    }
-});
-
-//Route for Delete a Book
-app.delete('/books/:id', async (request,response) =>{
-    try {
-        const { id } = request.params;
-
-        const result = await Book.findByIdAndDelete(id);
-        if(!result) {
-            return response.status(404).json({message:'Book not found' });
-        }
-        return response.status(200).send({message:'Book Deleted SuccessFullly'});
-    }catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message})
-    }
-});
-
-
-mongoose.connect(mongoDBURL).then(() => {
-    console.log('App connected to Database');
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to Database");
     app.listen(PORT, () => {
-        console.log(`App is Listening to port:${PORT}`);
-    })    
-}).catch((error) => {
+      console.log(`App is Listening to port:${PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
