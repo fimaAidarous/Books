b2c1bef8acb5e04be7a830ae4195c30e9b90aead
fimaAidarous <fma0899@gmail.com>
2023-12-25 03:54:41 -0800

    backend

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const app = express();
 
 app.use(express.json());
 
+// MidleWare for hadling CORS POlicy
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+
+// Option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:5577',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['content-type'],
+    })
+);
+
 app.get("/", (request, response) => {
   console.log(request);
   return request.status(234).send("Welcome to MERN Stack Tutorial");
